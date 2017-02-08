0d3936f219a6de0c6328b2d506f566e0a25e8dd2
Jay Schwane <jrschwane@uwalumni.com>
2017-02-07 22:55:51 -0600

    Refactor app.js, moving all logic to separate files; Add wallpapers

--- js/app.js
+++ js/app.js
@@ -1,90 +1,18 @@
 /* jshint esversion:6 */
 /* globals $, document, console */
 
+/* app.js call each module's public method(s)  */
+
 $(document).ready(function () {
+    
+    // Backgrounds.init();  // todo
+    
+    // Greeter.init();  // todo
+    
+    HitApi.getEvents();
+    
+    RenderTable.render();
 
-    // cache DOM elements
-    var $inputForm = $('#input-form');
-
-    // bind events
-    $inputForm.submit(getEvents);
-
-    // get github events
-    function getEvents(e) {
-
-        e.preventDefault();
-
-        var api = {
-            url   : 'https://api.github.com/repos',
-            user  : e.currentTarget[0].value,
-            repo  : e.currentTarget[1].value,
-            route : 'events'
-        };
-        var repoEvents = `${api.url}/${api.user}/${api.repo}/${api.route}`;
-
-        $.getJSON(repoEvents).then(handleResponse);
-    }
-
-    // handle GH JSON response data
-    function handleResponse(response) {
-
-        // we're splitting events into 3 categories, based on event type
-        var commitTypes = ['PushEvent',
-                           'CommitCommentEvent'
-                          ],
-            
-            todoTypes = ['IssuesEvent',
-                         'IssueCommentEvent',
-                         'ProjectEvent',
-                         'ProjectCardEvent',
-                         'ProjectColumnEvent'
-                        ],
-            
-            mergeTypes = ['CreateEvent',
-                          'PullRequestEvent',
-                          'PullRequestReviewEvent',
-                          'PullRequestReviewCommentEvent'
-                         ],
-            
-            // create array of commit-related events
-            commitEvents = response
-                .filter((e) => commitTypes.indexOf(e.type) !== -1)
-                .map(prepareEvents),
-
-            // create array of todo-related events
-            todoEvents = response
-                .filter((e) => todoTypes.indexOf(e.type) !== -1)
-                .map(prepareEvents),
-
-            // create build array of merge-related events
-            mergeEvents = response
-                .filter((e) => mergeTypes.indexOf(e.type) !== -1)
-                .map(prepareEvents);
-        
-        // call renderer
-        RenderTable.render(commitEvents, todoEvents, mergeEvents);
-        // RenderChart.render(commitEvents, todoEvents, mergeEvents);  // to do
-    }
-
-    // prepare events prior to rendering
-    function prepareEvents(e) {
-        return {
-            type    : e.type,
-            date    : formatDates(e.created_at),
-            actor   : e.actor.display_login,
-            avatar  : e.actor.avatar_url,
-            payload : e.payload
-        };
-    }
-
-    // format dates
-    function formatDates(date) {
-
-        return new Date(date) // make date object for day & month abbrev
-            .toDateString() // convert object to a string
-            .slice(0, 15) // remove everything after the YYYY
-            .replace(/ 0/g, ' '); // remove leading zeros (ex. '04' => '4')
-
-    }
+    // RenderGraph.render();  // todo
 
 });
