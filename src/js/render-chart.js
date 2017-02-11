68bb145642f6220fc52059656c1b5f1e6d368b80
Jay Schwane <jrschwane@uwalumni.com>
2017-02-11 17:05:48 -0600

    Mostly finish chart rendering module

--- src/js/render-chart.js
+++ src/js/render-chart.js
@@ -11,9 +11,9 @@ var RenderChart = (function () {
         // define chart layout attributes
         width   = 1000,
         height  = 400,
-        xMargin = 10,
-        yMargin = 10,
-        spacer  = 10;
+        xMargin = 80,
+        yMargin = 40,
+        spacer  = 40;
         
 
     // Cache DOM elements
@@ -32,7 +32,7 @@ var RenderChart = (function () {
     }
 
 
-    // Add Date prototype method
+    // Teach Date a new method
     Date.prototype.addDays = function(days) {
         var dat = new Date(this.valueOf());
         dat.setDate(dat.getDate() + days);
@@ -54,10 +54,10 @@ var RenderChart = (function () {
         }
 
         // fancy
-        console.log('========= DATE SPECS =========');
-        console.log('Days in range: ' + dateArray.length);
-        console.log('   Start date: ' + formatDates(startDate));
-        console.log('    Stop date: ' + formatDates(stopDate));
+//        console.log('========= DATE SPECS =========');
+//        console.log('Days in range: ' + dateArray.length);
+//        console.log('   Start date: ' + formatDates(startDate));
+//        console.log('    Stop date: ' + formatDates(stopDate));
         
         return dateArray;
     }
@@ -73,26 +73,13 @@ var RenderChart = (function () {
             return Math.max(a, b[1]);
         }, points[0][1]);
 
-        quotient = Math.floor(maxNumEvts / 5);
+        quotient = Math.floor(maxNumEvts / 4);
 
-        console.log('Max daily events: ' + maxNumEvts);
-        console.log('.. so Y axis max: ' + ((quotient + 1) * 5));
+//        console.log('Max daily events: ' + maxNumEvts);
+//        console.log('.. so Y axis max: ' + ((quotient + 1) * 4));
 
-        // return 1 more than the quotient, times 5
-        return ((quotient + 1) * 5);
-    }
-    
-    
-    // Main render method calls prepareDate if events.
-    // Receives ALL events from 'get-events.js'
-    function render(events) {
-        
-        // clear previous results
-        DOM.$container.empty();
-
-        if (events.length > 0) {
-            prepareData(events);
-        }
+        // return 1 more than the quotient, * 5
+        return ((quotient + 1) * 4);
     }
     
     
@@ -106,7 +93,7 @@ var RenderChart = (function () {
             sortedEvents,
             dateArray,
             chartData = [];
-
+        
         es.forEach(function (evt) {
 
             // console.log(evt);
@@ -130,9 +117,11 @@ var RenderChart = (function () {
             }
         });
         
+        // console.log(events);  // ok
+        
         // sort events ascending
         sortedEvents = events.sort( (a, b) => new Date(a.date) - new Date(b.date) );
-        // console.log(sortedEvents);
+        // console.log(sortedEvents); // ok
         
         // get array of all dates in range
         dateArray = buildDateRange(sortedEvents);
@@ -149,28 +138,20 @@ var RenderChart = (function () {
             ];            
         });
         
-        // console.log(chartData);  // FUCK YES
+        console.log(chartData);  // FUCK YES
         
-        // send data to chart builder
-        buildChart(chartData);
+        return chartData;
         
     }
     
     
-    // build the damn chart
-    function buildChart(points) {
-        
-        // getChartYMax(points);
-        
-        // add CSS class, namespace and attributes to main <svg> element
-        DOM.$svgElem.addClass('chart--content')
-            .attr('id', 'svg')
-            .attr('xmlns', ns)
-//            .attr('width', '100%')
-//            .attr('height', '100%')
-            .attr('viewBox', '0 0 ' + width + ' ' + height)
-            .attr('xml:space', 'preserve');
-        
+    /* generate chart points
+     *
+     * @params  [array]    points  [array of coordinate points]
+     * @returns [object]           [<g>roup of <circle> elements]
+    */
+    function generateCircles(points) {
+
         // init vars
         var group = $(document.createElementNS(ns, 'g')),
             currentPoint,
@@ -212,20 +193,247 @@ var RenderChart = (function () {
             circle.append(title);
             
             // append circle to group
-            group.append(circle);
+            group
+                .addClass('chart-circle')
+                .append(circle);
             
             
         });
+    
+    return group;
+    
+    }
+    
+    
+    /* generate chart line
+     *
+     * @params  [array]    points  [array of coordinate points]
+     * @returns [object]           [<g>roup of <line> segments]
+    */
+    function generateLines(points) {
+        
+        // init vars
+        var group = $(document.createElementNS(ns, 'g')),
+            currentPoint,
+            i,
+            line,
+            numPoints = points.length - 1,
+            chartYMax = getChartYMax(points),
+            
+            // multipliers scale the output x & y coords
+            xMultiple = (width - xMargin - spacer) / numPoints,
+            yMultiple = (height - yMargin - spacer) / chartYMax,
+            
+            // format values to align with chart extents
+            formattedPoints = points.map(function(point, ind) {
+                return [
+                    (ind * xMultiple) + xMargin,
+                    (height - (point[1] * yMultiple)) - yMargin
+                ];
+            });
+        
+        // loop through points array appending lines to group
+        for (i = 0; i < formattedPoints.length - 1; i += 1) {
+            
+            line = $(document.createElementNS(ns, 'line'));
+            
+            var x1 = formattedPoints[i][0],
+                y1 = formattedPoints[i][1],
+                x2 = formattedPoints[i + 1][0],
+                y2 = formattedPoints[i + 1][1];
+            
+            line
+                .attr('x1', x1)
+                .attr('y1', y1)
+                .attr('x2', x2)
+                .attr('y2', y2);
                 
+            // append circle to group
+            group
+                .addClass('chart-line')
+                .append(line);
+        }
+    
+    return group;
+    
+    }
+    
+    
+    /* generate axis labels
+     *
+     * @params  [array]    points  [array of coordinate points]
+     * @params  [string]   axis    ['y' or 'x' specifies conditional path]
+     * @returns [object]           [<g>roup of axis <line> and <text> els]
+    */
+    function genAxisLabels(points, axis) {
+        
+        var group = $(document.createElementNS(ns, 'g')),
+            line, // placeholder for <line> element built in loop
+            text, // placeholder for <text> element built in loop
+            numAxisTicks,
+            xPos, xValue, xMultiple,
+            yPos, yValue, ySpacing,
+            maxYval,
+            chartYMax = getChartYMax(points),
+            i;
+        
+        maxYval = points.reduce(function (a, b) {
+            return Math.max(a, b[1]);
+        }, points[0][1]);
+        
+        // conditionally handle passed axis param
+        if (axis === 'y') {
+            // add CSS class to <g> element
+            group.addClass('labels y-labels');
+            
+            // limit qty of Y axis labels based on chartYMax
+            numAxisTicks = (chartYMax > 12) ? chartYMax / 4 : chartYMax;
+            
+            // calculate relative space between each Y axis label
+            ySpacing = (height - yMargin - spacer) / numAxisTicks;
+            
+            // loop through numAxisTicks, generate <line> and <text> elements,
+            // then append each of them to our <g>roup
+            for (i = 0; i < numAxisTicks + 1; i += 1) {
+                
+                // calculate y axis label value
+                yValue = i * chartYMax / numAxisTicks;
+                
+                // calculate y axis label position
+                yPos = height - yMargin - (ySpacing * i);
+                
+                // create new <line> element
+                line = $(document.createElementNS(ns, 'line'));
+                
+                // add attributes to <line> element
+                line.addClass('horiz-rule')
+                    .attr('x1', xMargin)
+                    .attr('y1', yPos)
+                    .attr('x2', (width - spacer + 5))
+                    .attr('y2', yPos);
+                
+                // create new <text> element
+                text = $(document.createElementNS(ns, 'text'));
+                
+                // add attributes to <text> element
+                text
+                    .attr('x', (xMargin - 20))
+                    .attr('y', (yPos + 4))
+                    .html(yValue);
+                
+                group
+                    .append(line)
+                    .append(text);
+            }
+            return group;
+        
+        } else if (axis === 'x' || !axis || axis === null) {
+            
+            // add CSS class to the <g>roup
+            group.addClass('labels x-labels');
+            
+            numAxisTicks = points.length - 1;
+            xMultiple = (width - xMargin - spacer) / numAxisTicks;
+            
+            // loop through points, generate <line> and <text> elements,
+            // and append each of them to our <g> group
+            for (i = 0; i < points.length; i += 1) {
+                
+                // for large datasets, only append every 10th date
+                if (points.length > 10) {
+                    
+                    if (i % 10 === 0) {
+                        // calculate y axis label values
+                        xValue = points[i][0];
+
+                        // calculate y position
+                        xPos = i * xMultiple;
+
+                        // create new <text> element
+                        text = $(document.createElementNS(ns, 'text'));
+
+                        // add attributes to <text> element
+                        text
+                            .attr('x', (xPos + xMargin))
+                            .attr('y', (height - 5))
+                            .text(xValue.slice(3, 10));
+
+                        group
+                            .append(text);
+                    }
+                    
+                } else {                
+
+                    // calculate y axis label values
+                    xValue = points[i][0];
+
+                    // calculate y position
+                    xPos = i * xMultiple;
+
+                    // create new <text> element
+                    text = $(document.createElementNS(ns, 'text'));
+
+                    // add attributes to <text> element
+                    text
+                        .attr('x', (xPos + xMargin))
+                        .attr('y', (height - 5))
+                        .text(xValue.slice(3, 10));
+
+                    group
+                        .append(text);
+                }
+            }
+            return group;
+        }
+        
+    }
+
+    
+    // MAIN RENDERER
+    // Receives ALL events from 'get-events.js'
+    function render(events) {
+        
+        var points;
+        
+        // add CSS class, namespace and attributes to main <svg> element
         DOM.$svgElem
-            .empty()
-            .append(group);
+            .addClass('chart--content')
+            .attr('id', 'svg')
+            .attr('xmlns', ns)
+            // .attr('width', '100%')
+            .attr('height', '100%')
+            .attr('viewBox', '0 0 ' + width + ' ' + height)
+            .attr('xml:space', 'preserve');
         
-        DOM.$container.append(DOM.$svgElem);
+        if (events.length > 0) {
+            
+            // format events to usable points
+            points = prepareData(events);
         
-    }  /* end buildChart */
+            // empty parent SVG element before each render
+            DOM.$svgElem.empty();
 
+            // append axis label <text> <g>roups
+            DOM.$svgElem
+                .append(genAxisLabels(points, 'y'))
+                .append(genAxisLabels(points, 'x'));
+            
+            // append line
+            DOM.$svgElem
+                .append(generateLines(points));
 
+            // append points/circles <g>roup
+            DOM.$svgElem
+                .append(generateCircles(points));
+
+            // append SVG to container
+            DOM.$container
+                .append(DOM.$svgElem);
+            
+        }        
+    }
+    
+    
     // public init method
     function init() {
         cacheDom();
