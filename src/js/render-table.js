fc2b2302b54f1ac92801483457bcf6fb2aa411f5
Jay Schwane <jrschwane@uwalumni.com>
2017-02-25 18:57:17 -0600

    Fix error rendering multiple commits; DRY code cleanup

--- src/js/render-table.js
+++ src/js/render-table.js
@@ -42,97 +42,161 @@ var RenderTable = (function ($) {
             renderMergeEvents(mergeEvents);
         }
     }
-
-
+    
+    
+    // utility that generates repeated list DOM elements
+    function genListParts() {
+        return {
+            $listItem  : $(document.createElement('li')),
+            $img       : $(document.createElement('img')),
+            $paragraph : $(document.createElement('p')),
+            $paraSpan  : $(document.createElement('span')),
+            $a         : $(document.createElement('a'))
+        };
+    }
+    
+    
+    
+    /* assemble <a> DOM elements
+     *
+     * @params  [string]   url    [link url]
+     * @params  [string]   text   [link text]
+     * @returns [object]          [assembled <a> DOM object]
+    */
+    function buildAnchor(url, text) {
+        
+        return $(document.createElement('a'))
+            .attr('target', '_blank')
+            .attr('href', url)
+            .html(`<span class="bolder">${text}</span>`);
+    }
+    
+    
+    /* assemble table-col from list DOM elements
+     *
+     * @params  [number]   num       [number of events in column]
+     * @params  [string]   heading   [description of col's events]
+     * @params  [object]   list      [the list jquery object]
+     * @returns [object]             [assembled column DOM object]
+    */
+    function buildColumn(num, heading, list) {
+        return $(document.createElement('div'))
+            .addClass('table-col')
+            .append(`<h3>${num} ${heading}</h3>`)
+            .append(list);        
+    }
+    
+    
+    /* function to flatten list of commit-related events.
+     * Push Events include arrays of commits, which need
+     * to be pulled out separately if we're to append each
+     * commit to the list
+     *
+     * @params    [array]   es   [all events]
+     * @returns   [array]        [flattened array]
+    */
+    function flattenCommits(es) {
+        
+        var flattened = [];
+        
+        es.forEach(function (e) {
+            
+            if (e.type === 'PushEvent') {
+                // it's a Push Event, extract & push separate commits
+                e.payload.commits.forEach(function (commit) {
+                    flattened.push({
+                        actor   : e.actor,
+                        avatar  : e.avatar,
+                        date    : e.date,
+                        message : commit.message,
+                        sha     : commit.sha,
+                        type    : 'commit',
+                        url     : commit.url
+                    });
+                });
+                
+            } else {
+                // it's a comment - push straight to 'allEvents'
+                flattened.push(e);
+            }
+            
+        });
+        
+        return flattened;
+    }
+    
+    
     // render commit events
     function renderCommitEvents(es) {
 
         // receives PushEvent & CommitCommentEvent event types
 
-        var $listContainer = $(document.createElement('div')),
-            $column       = $(document.createElement('div')),
-            $list         = $(document.createElement('ul'));
-
-        es.forEach(function (evt) {
+        var allEvents = flattenCommits(es),
+            heading = 'Commit-Related Events',
+            $list   = $(document.createElement('ul'));
+        
+        allEvents.forEach(function (evt) {
             
             // console.log(evt);
             
-            var $listItem  = $(document.createElement('li')),
-                $img       = $(document.createElement('img')),
-                $paragraph = $(document.createElement('p')),
-                $paraSpan  = $(document.createElement('span')),
-                $a         = $(document.createElement('a'));
-            
-            $a
-                .attr('target', '_blank');
+            var list = genListParts(),
+                anchorUrl,
+                anchorText;
 
-            if (evt.type === 'PushEvent') {
+            if (evt.type === 'commit') {
 
-                // when it's a push event, extract the commits
-                evt.payload.commits.forEach(function (commit) {
+                // convert api URl to html URL
+                anchorUrl = (evt.url)
+                    .replace(/\/\/api./, '\/\/')    // remove 'api.'
+                    .replace(/\/repos\//, '\/')     // remove 'repos/'
+                    .replace(/commits/, 'commit');  // singular 'commit'
 
-                    // convert commit URL into regular github.com URL
-                    // from: https://api.github.com/repos/{user}/{repo}/commits/{sha}
-                    //   to: https://github.com/{user}/{repo}/commit/{sha}
-                    var cleanUrl = (commit.url)
-                        .replace(/\/\/api./, '\/\/')    // remove 'api.'
-                        .replace(/\/repos\//, '\/')     // remove 'repos/'
-                        .replace(/commits/, 'commit');  // singular 'commit'
+                anchorText = (evt.sha).slice(0,6);
 
-                    $img
-                        .attr('src', evt.avatar)
-                        .appendTo($listItem);
+                list.$img
+                    .attr('src', evt.avatar)
+                    .appendTo(list.$listItem);
 
-                    $a
-                        .attr('href', cleanUrl)
-                        .html(`<span class="bolder">${(commit.sha).slice(0,6)}</span>`);
+                list.$paraSpan
+                    .text(`"${evt.message}"`);
 
-                    $paraSpan
-                        .text(`"${commit.message}"`);
-
-                    $paragraph
-                        .append(`<span class="bolder">${evt.actor}</span> committed `);
-
-                });
+                list.$paragraph
+                    .append(`<span class="bolder">${evt.actor}</span> committed `);
 
             } else { // else it's a comment
+                
+                anchorUrl = evt.payload.comment.html_url;
+                
+                anchorText = (evt.payload.comment.commit_id).slice(0,6);
 
-                $img
+                list.$img
                     .attr('src', evt.avatar)
-                    .appendTo($listItem);
+                    .appendTo(list.$listItem);
 
-                $a
-                    .attr('href', evt.payload.comment.html_url)
-                    .html(`<span class="bolder">${(evt.payload.comment.commit_id).slice(0,6)}</span>`);
-
-                $paraSpan
+                list.$paraSpan
                     .text(`"${evt.payload.comment.body}"`);
 
-                $paragraph
+                list.$paragraph
                     .append(`<span class="bolder">${evt.actor}</span> commented on `);
 
             }
             
-            $paragraph
-                .append($a)
+            list.$paragraph
+                .append(buildAnchor(anchorUrl, anchorText))
                 .append(` on ${formatDate(evt.date)}: `)
-                .append($paraSpan)
-                .appendTo($listItem);
+                .append(list.$paraSpan)
+                .appendTo(list.$listItem);
             
-            $listItem
+            list.$listItem
                 .appendTo($list);
 
         });
 
         $list
-            .addClass('col-list')
-            .appendTo($listContainer);
-        
-        $column
-            .addClass('table-col')
-            .append(`<h3>${es.length} Commit-Related Events</h3>`)
-            .append($listContainer)
-            .appendTo(DOM.$tableContainer);
+            .addClass('col-list');
+
+        DOM.$tableContainer
+            .append(buildColumn(allEvents.length, heading, $list));
         
     }
 
@@ -143,61 +207,53 @@ var RenderTable = (function ($) {
         // gets IssuesEvents, IssueCommentEvents, ProjectEvents,
         //   ProjectCardEvents, and ProjectColumnEvents
 
-        var $column = $(document.createElement('div')),
+        var heading = 'Todo-Related Events',
             $list   = $(document.createElement('ul'));
 
         es.forEach(function (evt) {
 
             // console.log(evt);
 
-            var $listItem  = $(document.createElement('li')),
-                $img       = $(document.createElement('img')),
-                $paragraph = $(document.createElement('p')),
-                $a         = $(document.createElement('a'));
-
-            $img
+            var list = genListParts(),
+                anchorUrl,
+                anchorText;
+            
+            list.$img
                 .attr('src', evt.avatar)
-                .appendTo($listItem);
-
-            $a.attr('target', '_blank');
+                .appendTo(list.$listItem);
 
             if (evt.type === 'IssuesEvent') {
+                
+                anchorUrl  = evt.payload.issue.html_url;
+                anchorText = `Issue ${evt.payload.issue.number}`;
 
-                $a
-                    .attr('href', evt.payload.issue.html_url)
-                    .html(`<span class="bolder">issue ${evt.payload.issue.number}</span>`);
-
-                $paragraph
+                list.$paragraph
                     .append(`<span class="bolder">${evt.actor}</span> ${evt.payload.action} `)
-                    .append($a)
+                    .append(buildAnchor(anchorUrl, anchorText))
                     .append(` on ${formatDate(evt.date)}: "${evt.payload.issue.title}"`);
 
             } else if (evt.type === 'IssueCommentEvent') {
+                
+                anchorUrl  = evt.payload.comment.html_url;
+                anchorText = `Issue ${evt.payload.issue.number}`;
 
-                $a
-                    .attr('href', evt.payload.comment.html_url)
-                    .html(`<span class="bolder">issue ${evt.payload.issue.number}</span>`);
-
-                $paragraph
+                list.$paragraph
                     .append(`<span class="bolder">${evt.actor}</span> commented on `)
-                    .append($a)
+                    .append(buildAnchor(anchorUrl, anchorText))
                     .append(` on ${formatDate(evt.date)}: "${evt.payload.comment.body}"`);
 
             }
             
-            $listItem
-                .append($paragraph)
+            list.$listItem
+                .append(list.$paragraph)
                 .appendTo($list);
         });
 
         $list
             .addClass('col-list');
 
-        $column
-            .addClass('table-col')
-            .append(`<h3>${es.length} Todo-Related Events</h3>`)
-            .append($list)
-            .appendTo(DOM.$tableContainer);
+        DOM.$tableContainer
+            .append(buildColumn(es.length, heading, $list));
 
     }
 
@@ -208,89 +264,78 @@ var RenderTable = (function ($) {
         // receives CreateEvent, PullRequestEvent, PullRequestReviewEvent
         //   and PullRequestReviewCommentEvent event types
 
-        var $column = $(document.createElement('div')),
-            $list   = $(document.createElement('ul')),
-            cleanUrl;
+        var heading = 'Merge-Related Events',
+            $list   = $(document.createElement('ul'));
 
         es.forEach(function (evt) {
 
             // console.log(evt);
 
-            var $listItem  = $(document.createElement('li')),
-                $img       = $(document.createElement('img')),
-                $paragraph = $(document.createElement('p')),
-                $a         = $(document.createElement('a'));
+            var list = genListParts(),
+                anchorUrl,
+                anchorText;
 
-            $img.attr('src', evt.avatar);
-
-            $a.attr('target', '_blank');
+            list.$img.attr('src', evt.avatar);
 
             if (evt.type === 'CreateEvent') {
 
-                $paragraph
+                list.$paragraph
                     .append([
-                        `<span class="bolder">${evt.actor}</span>`,
-                        'created',
+                        `<span class="bolder">${evt.actor}</span> created`,
                         evt.payload.ref_type,
-                        `<span class="bolder">${evt.payload.ref}</span>`,
-                        'on',
+                        `<span class="bolder">${evt.payload.ref}</span> on`,
                         formatDate(evt.date)
                     ].join(' '));
 
             } else if (evt.type === 'PullRequestEvent') {
 
-                // from: https://api.github.com/repos/belcurv/meerkat_momentum/pulls/11
-                //   to: https://github.com/belcurv/meerkat_momentum/pull/11
-                cleanUrl = (evt.payload.pull_request.url)
+                // convert api URl to html URL
+                anchorUrl = (evt.payload.pull_request.url)
                     .replace(/\/\/api./, '\/\/')  // remove 'api.'
                     .replace(/\/repos\//, '\/')   // remove 'repos/'
                     .replace(/pulls/, 'pull');    // singular 'pull'
 
-                $a
-                    .attr('href', cleanUrl)
-                    .html(`<span class="bolder">pull request ${evt.payload.pull_request.number}</span>`);
+                anchorText = `Pull Request ${evt.payload.pull_request.number}`;
 
-                $paragraph
+                list.$paragraph
                     .append(`<span class="bolder">${evt.actor}</span>`)
                     .append(` ${evt.payload.action} `)
-                    .append($a)
+                    .append(buildAnchor(anchorUrl, anchorText))
                     .append(` on ${formatDate(evt.date)}`);
 
             } else if (evt.type === 'PullRequestReviewEvent') {
+                
+                anchorUrl  = evt.payload.pull_request.html_url;
+                anchorText = evt.payload.pull_request.title;
 
-                $a
-                    .attr('href', evt.payload.pull_request.html_url)
-                    .text(evt.payload.pull_request.title);
-
-                $paragraph
+                list.$paragraph
                     .append([
                         evt.actor,
                         evt.payload.action,
                         'review of '
                     ].join(' '))
-                    .append($a)
+                    .append(buildAnchor(anchorUrl, anchorText))
                     .append(' on' + formatDate(evt.date));
 
             } else if (evt.type === 'PullRequestReviewCommentEvent') {
+                
+                anchorUrl  = evt.payload.comment.html_url;
+                anchorText = evt.payload.comment.body;
 
-                $a
-                    .attr('href', evt.payload.comment.html_url)
-                    .text(evt.payload.comment.body);
-
-                $paragraph
+                list.$paragraph
                     .append([
                         `<span class="bolder">${evt.actor}</span>`,
                         evt.payload.action,
                         'pull request comment '
                     ].join(' '))
-                    .append($a)
+                    .append(buildAnchor(anchorUrl, anchorText))
                     .append(' on' + formatDate(evt.date));
 
             }
             
-            $listItem
-                .append($img)
-                .append($paragraph)
+            list.$listItem
+                .append(list.$img)
+                .append(list.$paragraph)
                 .appendTo($list);
 
         });
@@ -298,11 +343,8 @@ var RenderTable = (function ($) {
         $list
             .addClass('col-list');
 
-        $column
-            .addClass('table-col')
-            .append(`<h3>${es.length} Merge-Related Events</h3>`)
-            .append($list)
-            .appendTo(DOM.$tableContainer);
+        DOM.$tableContainer
+            .append(buildColumn(es.length, heading, $list));
         
     }
     
